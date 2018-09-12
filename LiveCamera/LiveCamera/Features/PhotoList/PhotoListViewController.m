dd0e6b60e9b814544b65cbc64ea4ab0550a57d95
Cstzhangzb <devzhangzb@163.com>
2018-09-12 17:24:55 +0800

    搜索指定相册图片

--- LiveCamera/LiveCamera/Features/PhotoList/PhotoListViewController.m
+++ LiveCamera/LiveCamera/Features/PhotoList/PhotoListViewController.m
@@ -9,15 +9,16 @@
 #import "PhotoListViewController.h"
 #import "PhotoViewModel.h"
 #import "DeviceModel.h"
+#import "RootNavigationController.h"
 @interface PhotoListViewController ()
 @property (strong, nonatomic) PhotoViewModel *photoViewModel;
+@property (strong, nonatomic) NSMutableArray *photoArray;
 @end
 
 @implementation PhotoListViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-
 }
 
 - (void)setupNavigationItems {
@@ -28,8 +29,10 @@
 - (void)viewWillAppear:(BOOL)animated{
      [super viewWillAppear:animated];
      [self checkPhoto];
+     [self searchPhotoAlbum];
 }
 
+#pragma mark - 检查是否有新拍的照片
 - (void)checkPhoto{
     NSDictionary *hostDic = [UserDefaultUtil objectForKey:@"HostArray"];
     if (hostDic.count == 0) {
@@ -39,8 +42,6 @@
         NSString * requst = [hostDic objectForKey:key];
         [self getPhoto:key token:requst];
     }
-    
-
 }
 
 
@@ -53,8 +54,10 @@ get photo
     if (![USER_INFO isLogin]) {
         return;
     }
+    ZBWeak;
     [self.photoViewModel setBlockWithReturnBlock:^(id returnValue) {
         [MBProgressHUD hideHUD];
+        [weakSelf searchPhotoAlbum];
     } WithErrorBlock:^(NSString *error) {
         [MBProgressHUD hideHUD];
     } WithFailureBlock:^(NSError *error) {
@@ -66,6 +69,49 @@ get photo
 }
 
 
+#pragma mark - 搜索指定相册照片
+/**
+ 搜索指定相册照片
+ */
+- (void)searchPhotoAlbum{
+    // 获得相机胶卷的图片
+    PHFetchResult<PHAssetCollection *> *collectionResult1 = [PHAssetCollection fetchAssetCollectionsWithType:PHAssetCollectionTypeAlbum subtype:PHAssetCollectionSubtypeAlbumRegular options:nil];
+    NSString *title = [NSBundle mainBundle].infoDictionary[(__bridge NSString *)kCFBundleNameKey];
+    for (PHAssetCollection *collection in collectionResult1) {
+        if (![collection.localizedTitle isEqualToString:title]) continue;
+        [self searchAllImagesInCollection:collection];
+        break;
+    }
+}
+
+
+/**
+ * 查询某个相册里面的所有图片
+ */
+- (void)searchAllImagesInCollection:(PHAssetCollection *)collection{
+    // 采取同步获取图片（只获得一次图片）
+    PHImageRequestOptions *imageOptions = [[PHImageRequestOptions alloc] init];
+    imageOptions.synchronous = YES;
+    
+    NSLog(@"相册名字：%@", collection.localizedTitle);
+    
+    // 遍历这个相册中的所有图片
+    PHFetchResult<PHAsset *> *assetResult = [PHAsset fetchAssetsInAssetCollection:collection options:nil];
+    for (PHAsset *asset in assetResult) {
+        // 过滤非图片
+        if (asset.mediaType != PHAssetMediaTypeImage) continue;
+        // 图片原尺寸
+        CGSize targetSize = CGSizeMake(asset.pixelWidth, asset.pixelHeight);
+        // 请求图片
+        [[PHImageManager defaultManager] requestImageForAsset:asset targetSize:targetSize contentMode:PHImageContentModeDefault options:imageOptions resultHandler:^(UIImage * _Nullable result, NSDictionary * _Nullable info) {
+            NSLog(@"图片：%@ %@", result, [NSThread currentThread]);
+            [self.photoArray addObject:result];
+        }];
+    }
+}
+
+
+#pragma mark - lazy func
 
 - (PhotoViewModel *)photoViewModel{
     if (!_photoViewModel) {
@@ -73,6 +119,8 @@ get photo
     }
     return _photoViewModel;
 }
+
+
 
 
 @end
